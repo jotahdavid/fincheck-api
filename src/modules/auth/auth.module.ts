08d6c141c0b1e2035d656d336937dcc72bd15375
Jhonatan David <82552187+jotahdavid@users.noreply.github.com>
2023-06-27 15:49:50 -0300

    feat: validate if enviroment variables are set

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -3,13 +3,14 @@ import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { DatabaseModule } from 'src/shared/database/database.module';
+import env from 'src/shared/config/env';
 
 @Module({
   imports: [
     DatabaseModule,
     JwtModule.register({
       signOptions: { expiresIn: '7d' },
-      secret: 'unsecure_jwt_secret',
+      secret: env.jwtSecret,
     }),
   ],
   controllers: [AuthController],
