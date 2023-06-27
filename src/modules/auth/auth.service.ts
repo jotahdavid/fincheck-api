e64501f1be9d76fbb390bc26b2a259b5ff8a64cf
Jhonatan David <82552187+jotahdavid@users.noreply.github.com>
2023-06-27 16:20:25 -0300

    refactor(auth): move user creation to signin

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,7 +1,12 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import { compare } from 'bcryptjs';
-import { AuthenticateDto } from './dto/authenticate.dto';
+import { compare, hash } from 'bcryptjs';
+import { SigninDto } from './dto/signin.dto';
+import { SignupDto } from './dto/signup.dto';
 import { UsersRepository } from 'src/shared/database/repositories/users/users.repository';
 
 @Injectable()
@@ -11,28 +16,67 @@ export class AuthService {
     private readonly jwtService: JwtService,
   ) {}
 
-  async authenticate(authenticateDto: AuthenticateDto) {
+  async authenticate(signinDto: SigninDto) {
     const user = await this.usersRepository.findByEmail({
-      email: authenticateDto.email,
+      email: signinDto.email,
     });
 
     if (!user) {
       throw new UnauthorizedException('Invalid credentials');
     }
 
-    const isPasswordValid = await compare(
-      authenticateDto.password,
-      user.password,
-    );
+    const isPasswordValid = await compare(signinDto.password, user.password);
 
     if (!isPasswordValid) {
       throw new UnauthorizedException('Invalid credentials');
     }
 
-    const acessToken = await this.jwtService.signAsync({
-      sub: user.id,
-    });
+    const acessToken = await this.generateAccessToken(user.id);
 
     return { acessToken };
+  }
+
+  async signup(signupDto: SignupDto) {
+    const isEmailTaken = await this.usersRepository.checkIfUserExistsByEmail({
+      email: signupDto.email,
+    });
+
+    if (isEmailTaken) {
+      throw new ConflictException('This e-mail is already in use');
+    }
+
+    const hashedPassword = await hash(signupDto.password, 8);
+
+    const user = await this.usersRepository.create({
+      name: signupDto.name,
+      email: signupDto.email,
+      password: hashedPassword,
+      categories: [
+        // Income
+        { name: 'Salário', icon: 'salary', type: 'INCOME' },
+        { name: 'Freelance', icon: 'freelance', type: 'INCOME' },
+        { name: 'Outro', icon: 'other', type: 'INCOME' },
+        // Expense
+        { name: 'Casa', icon: 'home', type: 'EXPENSE' },
+        { name: 'Alimentação', icon: 'food', type: 'EXPENSE' },
+        { name: 'Educação', icon: 'education', type: 'EXPENSE' },
+        { name: 'Lazer', icon: 'fun', type: 'EXPENSE' },
+        { name: 'Mercado', icon: 'grocery', type: 'EXPENSE' },
+        { name: 'Roupas', icon: 'clothes', type: 'EXPENSE' },
+        { name: 'Transporte', icon: 'transport', type: 'EXPENSE' },
+        { name: 'Viagem', icon: 'travel', type: 'EXPENSE' },
+        { name: 'Outro', icon: 'other', type: 'EXPENSE' },
+      ],
+    });
+
+    const acessToken = await this.generateAccessToken(user.id);
+
+    return { acessToken };
+  }
+
+  private generateAccessToken(userId: string) {
+    return this.jwtService.signAsync({
+      sub: userId,
+    });
   }
 }
--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
-import { DatabaseModule } from 'src/shared/database/database.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [],
   controllers: [UsersController],
   providers: [UsersService],
 })
