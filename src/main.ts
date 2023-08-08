1c730ae6d52f331e5612f45f04941015680cd286
Jhonatan David <82552187+jotahdavid@users.noreply.github.com>
2023-08-08 12:49:59 -0300

    feat: add allowed origins by .env

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,14 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import env from './shared/config/env';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
-    origin: '*',
+    origin: env.allowedOrigins,
   });
 
   await app.listen(process.env.PORT ?? 3000);
--- src/shared/config/env.ts
+++ src/shared/config/env.ts
@@ -1,15 +1,34 @@
-import { IsNotEmpty, IsString, NotEquals, validateSync } from 'class-validator';
-import { plainToInstance } from 'class-transformer';
+import {
+  IsNotEmpty,
+  IsString,
+  IsUrl,
+  NotEquals,
+  ValidateIf,
+  validateSync,
+} from 'class-validator';
+import { Transform, plainToInstance } from 'class-transformer';
 
 class Env {
   @IsString()
   @IsNotEmpty()
   @NotEquals('unsecure_jwt_secret')
   jwtSecret: string;
+
+  @ValidateIf((_, value) => value !== '*')
+  @IsString({ each: true })
+  @IsUrl({ require_tld: false, require_protocol: true }, { each: true })
+  @IsNotEmpty()
+  @Transform((params) => {
+    if (params.value === '*') return params.value;
+    const result = params.value?.split(';');
+    return result ? result : [];
+  })
+  allowedOrigins: string | string[];
 }
 
 const env = plainToInstance(Env, {
   jwtSecret: process.env.JWT_SECRET,
+  allowedOrigins: process.env.ALLOWED_ORIGINS,
 });
 
 const errors = validateSync(env);
