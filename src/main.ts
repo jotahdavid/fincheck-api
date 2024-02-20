37ccae97780e0f2c7f49aa81adea0294d2e92583
Jhonatan David <82552187+jotahdavid@users.noreply.github.com>
2024-02-19 21:21:39 -0300

    chore: log requests

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import * as morgan from 'morgan';
 
 import { AppModule } from './app.module';
 import env from './shared/config/env';
+import { Request } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +15,7 @@ async function bootstrap() {
   app.enableCors({
     origin: env.allowedOrigins,
   });
+  morgan.token('remote-addr', (req: Request) => req.headers['x-forwarded-for'] || req.connection.remoteAddress);
   app.use(morgan('combined'));
 
   await app.listen(process.env.PORT ?? 3000);
